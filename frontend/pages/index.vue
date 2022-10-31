763728bfdaca673d3798230d4cdc2ac49cf1d6aa
Tetsuro Kawakubo <tetsuro.riverkubo322@gmail.com>
2022-10-31 22:05:04 +0900

    feat: tailwindでstyle

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div>TikTok大喜利</div>
-    <div>
+    <div class="text-center mb-4 font-bold">TikTok大喜利</div>
+    <div class="text-center mb-4">
       <button><NuxtLink to="/ranking">ランキングを見る</NuxtLink></button>
     </div>
-    <div>
+    <div class="text-center mb-4">
       <button>
         <NuxtLink to="/tiktok-list"
           >大喜利するTikTok動画を一覧から選ぶ</NuxtLink
